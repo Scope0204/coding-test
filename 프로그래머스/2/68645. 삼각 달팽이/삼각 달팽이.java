428a4009892c1a3fd80b38812bb03486a9843a4f
JunGyeongCho <scope0204@gmail.com>
2025-01-02 16:31:56 +0900

    [level 2] Title: 삼각 달팽이, Time: 25.23 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,39 +1,29 @@
 class Solution {
+    private static final int[] dx = {0,1,-1};
+    private static final int[] dy = {1,0,-1}; 
+    
     public int[] solution(int n) {
+        
         int [][] arr = new int[n][n];
         int num = 1;
         int x=0;
         int y=0;
+        int d=0; // 방향 변수
 
         while(true) {
-            // 1. 아래로 이동
-            while(true) {
-                arr[y][x] = num++;
-                if(y + 1 == n || arr[y + 1][x] != 0) break;
-                y++;
+            arr[y][x] = num++;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if(nx == n || ny == n || nx == -1 || ny == -1 || arr[ny][nx] != 0){ // 더 이상 갈 수있는 방향이 없는 경우
+                d = (d+1) % 3; // 방향 전환
+                nx = x + dx[d];
+                ny = y + dy[d];
+                if(nx == n || ny == n || nx == -1 || ny == -1 || arr[ny][nx] != 0) break;
             }
-            if(x + 1 == n || arr[y][x + 1] != 0) break; // 오른쪽으로 이동할 칸이 없는 경우 반복문 종료
-            x++;
-
-            // 2. 오른쪽으로 이동
-            while(true) {
-                arr[y][x] = num++;
-                if(x + 1 == n || arr[y][x + 1] != 0) break;
-                x++;
-            }
-            if(arr[y - 1][x - 1] != 0) break;
-            x--;
-            y--;
-
-            // 3. 왼쪽 위로 이동
-            while(true) {
-                arr[y][x] = num++;
-                if(arr[y - 1][x - 1] != 0) break;
-                x--;
-                y--;
-            }
-            if(y + 1 == n || arr[y + 1][x] != 0) break;
-            y++;
+            // 좌표 이동
+            x = nx;
+            y = ny;
+            
         }
 
         int[] result = new int[num-1]; // 추가된 숫자-1 만큼 존재하므로 해당 값으로 배열 크기 지정
